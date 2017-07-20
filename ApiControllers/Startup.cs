2e1282fb9ce1afa2db0d420ff8893db041dcc4f7
Ali <ali62n62@yahoo.com>
2017-07-20 14:48:11 +0430

    structuremap configured

--- ApiControllers/Startup.cs
+++ ApiControllers/Startup.cs
@@ -18,18 +18,19 @@ namespace ApiControllers
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           // services.AddSingleton<IRepository, MemoryRepository>();
             services.AddMvc();
         }
 
         public void ConfigureContainer(Registry registry)
         {
-            // Use StructureMap-specific APIs to register services in the registry.
-            registry.For<IRepository>().Use<MemoryRepository>();
+            registry.For<IRepository>().Singleton().Use<MemoryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseStatusCodePages();
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
