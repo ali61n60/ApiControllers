08c764096ef4856cbb70dc9260a798ace572abc9
Ali <ali62n62@yahoo.com>
2017-07-21 21:50:12 +0430

    adding a new controller and working with route template in the method

--- ApiControllers/Controllers/ContentController.cs
+++ ApiControllers/Controllers/ContentController.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model;
 
 namespace ApiControllers.Controllers
 {
-    public class ContentController
+    [Route("api/[controller]")]
+    public class ContentController : Controller
     {
+        [HttpGet("ali")]
+        public string GetString() => "This is a string response";
+
+        [HttpGet("reza")]
+        public Reservation GetObject() => new Reservation {
+            ReservationId = 100,
+            ClientName = "Joe",
+            Location = "Board Room"
+        };
     }
 }
