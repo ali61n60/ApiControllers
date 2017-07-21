e480f5d09c9573e79ab76ce1adb68528e7b935a8
Ali <ali62n62@yahoo.com>
2017-07-21 18:31:12 +0430

    getting json data into android app

--- ApiCaller/MainActivity.cs
+++ ApiCaller/MainActivity.cs
@@ -1,7 +1,12 @@
-﻿using Android.App;
+﻿using System;
+using System.IO;
+using System.Json;
+using System.Net;
+using System.Threading.Tasks;
+using Android.App;
 using Android.Widget;
 using Android.OS;
-
+//https://developer.xamarin.com/recipes/android/web_services/consuming_services/call_a_rest_web_service/
 namespace ApiCaller
 {
     [Activity(Label = "ApiCaller", MainLauncher = true, Icon = "@drawable/icon")]
@@ -22,12 +27,40 @@ namespace ApiCaller
         private void initFields()
         {
             _button1 = FindViewById<Button>(Resource.Id.button1);
-            _button1.Click += (sender, args) =>
-            {
-                _numberOfClicks++;
-                _textView1.Text = "Number of clicks: " + _numberOfClicks;
+            _button1.Click += async (sender, e) => {
+
+                // Get the latitude and longitude entered by the user and create a query.
+                string url = "http://192.168.42.76/api/Reservation";
+
+                // Fetch the weather information asynchronously, 
+                // parse the results, then update the screen:
+                JsonValue json = await FetchWeatherAsync(url);
+                _textView1.Text = json.ToString();
+                // ParseAndDisplay (json);
             };
             _textView1 = FindViewById<TextView>(Resource.Id.textView1);
+        }
+        private async Task<JsonValue> FetchWeatherAsync(string url)
+        {
+            // Create an HTTP web request using the URL:
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            // Send the request to the server and wait for the response:
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                // Get a stream representation of the HTTP web response:
+                using (Stream stream = response.GetResponseStream())
+                {
+                    // Use this stream to build a JSON document object:
+                    JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+                    Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+                    // Return the JSON document:
+                    return jsonDoc;
+                }
+            }
         }
     }
 }
